57ca7c3d8c2c6c66af5d8a67a84b8ba7e3c43341
KimSeungHwan <35340846+Seunghwan96@users.noreply.github.com>
2022-11-20 20:08:20 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -0,0 +1,69 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.topNode = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.length) {
+      newNode.next = null;
+      this.topNode = newNode;
+    } else {
+      newNode.next = this.topNode;
+      this.topNode = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (!this.length) return -1;
+
+    const popedNode = this.topNode;
+    this.topNode = popedNode.next;
+    this.length--;
+    return popedNode.value;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+let answer = "";
+const count = Number(input[0]);
+
+for (let idx = 1; idx <= count; idx++) {
+  const stack = new Stack();
+  const parentheses = input[idx].split("");
+  try {
+    for (const data of parentheses) {
+      if (data === "(") stack.push(data);
+      else if (data === ")") {
+        const result = stack.pop();
+        if (result === -1) {
+          throw "NO";
+        }
+      }
+    }
+    if (stack.size()) {
+      throw "NO";
+    } else {
+      answer += "YES\n";
+    }
+  } catch (err) {
+    answer += "NO\n";
+  }
+}
+
+console.log(answer);
