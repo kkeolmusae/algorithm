a25e47618d582cf241d7da61b15deb7d0fed55b0
KimSeungHwan <35340846+Seunghwan96@users.noreply.github.com>
2022-12-02 14:15:36 +0900

    [Bronze I] Title: 소인수분해, Time: 408 ms, Memory: 9880 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -0,0 +1,22 @@
+/**
+ * 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+ */
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+function solution(input) {
+  let num = input;
+  let answer = [];
+  let i = 2;
+  while (num > 1) {
+    let tmp = num % i;
+    if (!tmp) {
+      answer.push(i);
+      num /= i;
+    } else {
+      i++;
+    }
+  }
+  return answer.join("\n");
+}
+console.log(solution(input));
