326d9c1c2403a2ec39000fa56060d02ce30c9f6b
Seunghwan <kim03208@naver.com>
2022-12-04 03:03:12 +0900

    Todo 정리

---
+++ backjoon/Todo/11052.js
@@ -0,0 +1,33 @@
+/**
+ * 입력
+ * 첫째 줄에 민규가 구매하려고 하는 카드의 개수 N이 주어진다. (1 ≤ N ≤ 1,000)
+ * 둘째 줄에는 Pi가 P1부터 PN까지 순서대로 주어진다. (1 ≤ Pi ≤ 10,000)
+ *
+ * 출력
+ * 첫째 줄에 민규가 카드 N개를 갖기 위해 지불해야 하는 금액의 최댓값을 출력한다.
+ */
+
+const input = require("fs").readFileSync("../input.txt").toString().trim().split("\n");
+
+function solution(input) {
+  const N = input[0];
+  const card = input[1].split(" ");
+  const dp = {};
+
+  // 기본값
+  dp[0] = 0;
+  for (let i = 1; i <= N; i++) {
+    dp[i] = Number(card[i - 1]);
+  }
+
+  // i = 살 수 있는 카드 수
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= i; j++) {
+      // 카드 j 장을 샀을때 나머지 살 수 있는 카드 -> i - j
+      dp[i] = Math.max(dp[i], dp[i - j] + dp[j]);
+    }
+  }
+
+  return dp[N];
+}
+console.log(solution(input));
---
+++ backjoon/Todo/9095.js
@@ -0,0 +1,36 @@
+/**
+ * 정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다.
+ * 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
+ * 1+1+1+1
+ * 1+1+2
+ * 1+2+1
+ * 2+1+1
+ * 2+2
+ * 1+3
+ * 3+1
+ * 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+ *
+ * -> dp[6] 까지 보고 규칙으로 해결
+ */
+
+const input = require("fs").readFileSync("../input.txt").toString().trim().split("\n");
+
+function solution(input) {
+  const maxTestCast = Math.max(...input);
+
+  const answer = [];
+  const dp = {};
+  dp[0] = 1; // ?
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+  for (let i = 4; i <= maxTestCast; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+  for (let i = 1; i < input.length; i++) {
+    answer.push(dp[input[i]]);
+  }
+
+  return answer.join("\n");
+}
+console.log(solution(input));
