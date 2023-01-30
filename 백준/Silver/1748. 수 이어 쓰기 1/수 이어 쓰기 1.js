1795126e229a1e343faec532570b6a644252c7b3
kkeolmusae <35340846+kkeolmusae@users.noreply.github.com>
2023-01-30 23:32:31 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 148 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.js
@@ -0,0 +1,24 @@
+/**
+ * 1부터 N까지의 수를 이어서 쓰면 다음과 같이 새로운 하나의 수를 얻을 수 있다.
+ * 1234567891011121314151617181920212223...
+ * 이렇게 만들어진 새로운 수는 몇 자리 수일까? 이 수의 자릿수를 구하는 프로그램을 작성하시오.
+ */
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+function solution(input) {
+  const num = Number(input);
+  if (num < 10) return num;
+
+  let answer = 0;
+  let numsCount = 9;
+  let tmp = "";
+  for (let i = 1; i < input.length; i++) {
+    answer += i * numsCount;
+    numsCount *= 10;
+    tmp += "9";
+  }
+  answer += input.length * (num - tmp);
+  return answer;
+}
+console.log(solution(input));
