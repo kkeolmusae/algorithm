40de838c81de200fdee45de5f771cb96934c8c46
sh kim <kim03208@naver.com>
2024-01-10 23:15:59 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -0,0 +1,29 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  let [N, K] = input[0].split(" ").map(Number);
+  const coins = input
+    .slice(1, input.length)
+    .map(Number)
+    .sort((a, b) => {
+      return b - a;
+    });
+
+  let count = 0;
+
+  for (const coin of coins) {
+    if (coin > K) continue;
+
+    if (K % coin === 0) {
+      count += K / coin;
+      break;
+    }
+
+    count += Math.floor(K / coin);
+    K -= coin * Math.floor(K / coin);
+  }
+
+  return count;
+}
+
+console.log(solution(input));
