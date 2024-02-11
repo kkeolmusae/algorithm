df5761052f5269b64b7d5ee3804a09da98c65dd8
sh kim <kim03208@naver.com>
2024-02-12 00:50:17 +0900

    [level 2] Title: 피보나치 수, Time: 19.88 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    constant = 1234567
+    
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % constant
+    return dp[i]
