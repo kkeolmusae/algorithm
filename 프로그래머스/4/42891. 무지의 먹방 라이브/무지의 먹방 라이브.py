46ced4b72854540528903c8ae78ffd6c957f64d3
sh kim <kim03208@naver.com>
2024-03-03 05:30:06 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 138.01 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -6,29 +6,26 @@ def solution(food_times, k):
         return -1
 
     q = []
-    # (음식먹는데 걸리는 시간, 음식 순서) 로 heapq에 넣음
     for i in range(len(food_times)):
         heapq.heappush(q, (food_times[i], i + 1))
 
-    sum_time_to_eat = 0  # 음식을 먹는데 걸린 총 시간
-    remaining_food_count = len(food_times)  # 남은 음식의 개수
-    prev = 0  # 이전 음식을 먹는데 걸린 시간
-    # (다음 음식을 먹는데 걸리는 시간을 계산할때 이전 음식을 먹을때 걸린 시간만큼 빼줘야함)
+    prev_food_time = 0  # 이전 음식 먹는데 걸린 시간
+    food_count = len(food_times)  # 남은 음식의 개수
 
     while True:
-        current_food_time = q[0][0]
-        time_to_eat = (current_food_time - prev) * remaining_food_count
-        if sum_time_to_eat + time_to_eat > k:
+        # 한번에 처리하고자 하는 개수
+        bulk_size = (q[0][0] - prev_food_time) * food_count
+
+        if bulk_size > k:
             break
-        heapq.heappop(q)
-        sum_time_to_eat += time_to_eat
-        remaining_food_count -= 1
-        prev = current_food_time
 
-    q.sort(key=lambda x: x[1])  # 남은 음식들 idx 순서대로 정렬
+        current_food = heapq.heappop(q)  # [0]: time, [1]: order
+        k -= bulk_size
+        food_count -= 1
+        prev_food_time = current_food[0]
 
-    # 다음 차례에 먹을 음식 => (k - sum_time_to_eat) % remaining_food_count
-    return q[(k - sum_time_to_eat) % remaining_food_count][1]
+    q.sort(key=lambda x: x[1])  # order 순 정렬
+    return q[k % len(q)][1]
 
 
-print(solution([3, 1, 2], 5))
+print(solution([3, 5, 1, 6, 5, 3], 20))
