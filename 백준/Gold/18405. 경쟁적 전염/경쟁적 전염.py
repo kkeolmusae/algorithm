51e7e6054f0de73d81aec58ed453534706cfae9c
sh kim <kim03208@naver.com>
2024-04-13 01:38:44 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34312 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,69 +1,62 @@
 """
-입력
-첫째 줄에 자연수 N, K가 공백을 기준으로 구분되어 주어진다. (1 ≤ N ≤ 200, 1 ≤ K ≤ 1,000) 
-둘째 줄부터 N개의 줄에 걸쳐서 시험관의 정보가 주어진다. 
-각 행은 N개의 원소로 구성되며, 해당 위치에 존재하는 바이러스의 번호가 공백을 기준으로 구분되어 주어진다. 
-단, 해당 위치에 바이러스가 존재하지 않는 경우 0이 주어진다. 
-또한 모든 바이러스의 번호는 K이하의 자연수로만 주어진다. 
-N+2번째 줄에는 S, X, Y가 공백을 기준으로 구분되어 주어진다. (0 ≤ S ≤ 10,000, 1 ≤ X, Y ≤ N)
+A17 경쟁전 전염 (난이도 2)
+각 바이러스가 낮은 번호 부터 증식한다 -> deque 사용
 
-출력
-S초 뒤에 (X,Y)에 존재하는 바이러스의 종류를 출력한다.
-만약 S초 뒤에 해당 위치에 바이러스가 존재하지 않는다면, 0을 출력한다.
 """
+
+from collections import deque
 
 
 def solution():
     n, k = map(int, input().split())
 
-    graph = []
-    for _ in range(n):
-        graph.append(list(map(int, input().split())))
+    graph = []  # 전체 보드 정보를 담는 리스트
+    data = []  # 바이러스에 대한 정보를 담는 리스트
 
-    pending_virus = [[] for _ in range(k + 1)]
     for i in range(n):
+        # 보드 정보를 한 줄 단위로 입력
+        graph.append(list(map(int, input().split())))
         for j in range(n):
+            # 해당 위치에 바이러스가 존재하는 경우
             if graph[i][j] != 0:
-                pending_virus[graph[i][j]].append((i, j))
+                # (바이러스 종류, 시간, 위치x, 위치 y) 삽인
+                data.append((graph[i][j], 0, i, j))
 
-    s, x, y = map(int, input().split())
+    # 정렬 이후에 큐로 옮기기(낮은 번호의 바이러스가 먼저 증식하기 떄문에)
+    data.sort()
+    q = deque(data)
 
-    # 4가지 이동 방향에 대한 리스트
-    # 북, 동, 남, 서
+    target_s, target_x, target_y = map(int, input().split())
+
+    # 바이러스가 퍼져나갈 수 있는 4가지 위치
+    # 북, 동, 남, 서 순서
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
 
-    def bfs(time):
-        virus_turn = 1
-        while time > 0:
-            if virus_turn == k + 1:
-                time -= 1
-                virus_turn = 1
-                continue
+    # bfs
+    while q:
+        (
+            virus,
+            s,
+            x,
+            y,
+        ) = q.popleft()
+        # 정확히 s 초가 지나거나 큐가 빌떄까지 반복
+        if s == target_s:
+            break
 
-            if len(pending_virus[virus_turn]) == 0:
-                virus_turn += 1
-                continue
+        # 현재 노드에서 주변 4가지 위치를 각각 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 해당 위치로 이동할 수 있는 경우
+            if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                # 아직 방문안한 위치라면, 그 위치에 바이러스 넣기
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = virus
+                    q.append((virus, s + 1, nx, ny))
 
-            temp = []
-            while len(pending_virus[virus_turn]):
-                vx, vy = pending_virus[virus_turn].pop(0)
-
-                for idx in range(4):
-                    nx = vx + dx[idx]
-                    ny = vy + dy[idx]
-
-                    if nx >= 0 and nx < n and ny >= 0 and ny < n:
-                        if graph[nx][ny] == 0:
-                            graph[nx][ny] = virus_turn
-                            temp.append((nx, ny))
-            pending_virus[virus_turn] = temp
-
-            # 다음 바이러스 차례
-            virus_turn += 1
-
-    bfs(s)
-    return graph[x - 1][y - 1]
+    return graph[target_x - 1][target_y - 1]
 
 
 print(solution())
