b33e080a0cb760a6f34e559ddc37aafc9fa501ac
sh kim <kim03208@naver.com>
2024-08-06 08:55:42 +0900

    Update

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -7,7 +7,7 @@ def solution(numbers):
     arr = [True] * (max_num + 1) # 소수: True
     arr[0]=False
     arr[1]= False
-    for i in range(2, int((max_num+1)**0.5) + 1):
+    for i in range(2, int((max_num+1)**0.5) + 1): # 에라토스테네스의 체
         if arr[i] == False:
             continue
         for j in range(i+i, max_num + 1, i):
@@ -15,8 +15,10 @@ def solution(numbers):
     
     cnt = 0
     l = []
-    for n in range(1, len(numbers) + 1):
+    # number로 만들 수 있는 숫자 조합 구하기
+    for n in range(1, len(numbers) + 1): 
         l += list(permutations(list(numbers), n))
+        
     s = set()
     for num in l:
         n = int("".join(num))
