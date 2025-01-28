85e9e0dadc1ae7d1a2133823626e955fb63feb4b
sh kim <kim03208@naver.com>
2025-01-29 00:26:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.59 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    if n < 3:
+        return n
+
+    dp = [0] * (n + 1)  # n 까지 도달할 수 있는 방법
+    dp[0] = 0
+    dp[1] = 1  # 0 에서 한칸
+    dp[2] = 2  # 0 에서 한칸, 두칸 방법
+
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+
+    return dp[n]
